766d99fb312f05323d05f00da21e7449908c0c84
Chelsea Heredia <chelseah2020@gmail.com>
2025-07-15 17:13:44 -0500

    Initializing backend

---
+++ backend/src/test/java/com/bookstacker/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookstacker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
